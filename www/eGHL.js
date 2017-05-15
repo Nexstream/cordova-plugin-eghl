57c92b4b1dd4cc36e072eec68f52eecc4bc018e6
TJ Woon <tj@cloudsky.org>
2017-05-15 12:14:31 +0800

    Updated plugin version to v3.0.7

--- www/eGHL.js
+++ www/eGHL.js
@@ -5,7 +5,7 @@ var argscheck = require('cordova/argscheck'),
 
 var eGHL = function() {
     this.name = 'eGHL Cordova Plugin';
-    this.version = '3.0.6'
+    this.version = '3.0.7'
 };
 
 eGHL.prototype = {
