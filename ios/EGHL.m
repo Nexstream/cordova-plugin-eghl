75a5f5a1e0dfa0595bf61ca077e67bd40fd5ee35
TJ Woon <tjwoon@appxtream.com>
2018-05-08 15:55:01 +0800

    [iOS] Accept HashValue from client

--- ios/EGHL.m
+++ ios/EGHL.m
@@ -70,6 +70,7 @@
             @"BillRegion",
             @"BillCountry",
             @"eghlDebugURL",
+            @"HashValue",
             @"PromoCode",
             @"ShipAddr",
             @"ShipPostal",
